048d818538669aeeb1554e283fa3ef314205c1dc
Camilla van Klinken <camillavk@gmail.com>
2014-09-22 18:26:07 +0100

    Passenger can topup balance

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -7,6 +7,7 @@ describe Station do
 
 let (:station) {Station.new}
 let (:train) {double :train}
+let (:passenger) {double :passenger, balance: 20}
 
 	it "should be able to dock trains" do
 		expect{station.dock(train)}.to change{station.train_count}.by 1
@@ -23,6 +24,10 @@ let (:train) {double :train}
 		origin.dock(train)
 		origin.transfer(destination)
 		expect(destination.train_count).to eq(1)
+	end
+
+	it "should be able to check Passenger balance" do
+		expect(station.balance_check(passenger)).to eq(20)
 	end
 
 end
