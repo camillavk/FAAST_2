a025d993758eb779c607feb3955db2eaed256985
Camilla van Klinken <camillavk@gmail.com>
2014-09-22 17:30:57 +0100

    Train throws an error when it has too many carriages

--- lib/train.rb
+++ lib/train.rb
@@ -1,9 +1,12 @@
 class Train
 
-	include PassengerHolder
+	DEFAULT_CAPACITY = 5
+	DEFAULT_CARRIAGES = 1
 
-	def initialize
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
 		@carriages = []
+		# options.fetch(:num_carriages, DEFAULT_CARRIAGES).times {dock(Carriage.new)}
 	end
 
 	def dock(carriage)
@@ -12,6 +15,10 @@ class Train
 
 	def carriage_count
 		@carriages.count 
+	end
+
+	def full?
+		raise "Cannot take more carriages" if carriage_count == DEFAULT_CAPACITY
 	end
 
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -5,9 +5,22 @@ describe Train do
 let (:train) {Train.new}
 let (:carriage) {double :carriage}
 
+	# it "should be able to initialize with carriages" do
+	# 	train = Train.new(:num_carriages => 5)
+	# 	expect(train.carriage_count).to eq(5)
+	# end
+	def fill_train(carriage)
+		5.times {train.dock(carriage)}
+	end
+
 	it "should be able to dock carriages" do
 		train.dock(carriage)
 		expect(train.carriage_count).to eq(1)
+	end
+
+	it "should throw an error when it is full" do
+		fill_train(carriage)
+		expect(lambda{train.full?}).to raise_error"Cannot take more carriages"
 	end
 
 end
